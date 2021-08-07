468b580ee1bd230f67a254d77b326e4df6604fd9
Siddhant Hullur <siddhanthullur@gmail.com>
2021-08-07 12:48:57 -0700

    Initial Commit

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,12 +1,22 @@
 
 #include "micromouseserver.h"
 
-
-
 void microMouseServer::studentAI()
 {
 
-    static int count = 0;
+    static int count;
+    static int grid [20][20];
+    static int direction;
+    /*  0
+     *3   1
+     *  2
+     */
+    static int x;
+    static int y;
+
+    static int left;
+    static int right;
+    static int forward;
 /*
  * The following are the eight functions that you can call. Feel free to create your own fuctions as well.
  * Remember that any solution that calls moveForward more than once per call of studentAI() will have points deducted.
@@ -55,42 +65,71 @@ void microMouseServer::studentAI()
     }
     */
 
-    if (!isWallForward() && isWallLeft() && isWallRight())
-    {
-        moveForward();
-        count = 0;
+    grid[x][y]++;
+
+    if (direction == 0) {
+        left = grid[x-1][y];
+        right = grid[x+1][y];
+        forward = grid[x][y+1];
+    } else if (direction = 1) {
+        left = grid[x][y+1];
+        right = grid[x][y-1];
+        forward = grid[x+1][y];
+    } else if (direction = 2) {
+        left = grid[x+1][y];
+        right = grid[x-1][y];
+        forward = grid[x][y-1];
+    } else if (direction = 3) {
+        left = grid[x][y-1];
+        right = grid[x][y+1];
+        forward = grid[x-1][y];
     }
 
-    if (isWallLeft() && !isWallRight())
-    {
-        turnRight();
-        moveForward();
-        count = 0;
-    }
-
-    if (!isWallLeft() && isWallRight())
-    {
-        turnLeft();
-        moveForward();
-        count = 0;
-    }
-
-    if (isWallRight() && isWallForward())
-    {
-        turnLeft();
-        moveForward();
-        count = count + 1;
-    }
-    if (isWallLeft() && isWallForward())
-    {
-        turnRight();
-        moveForward();
-        count = count + 1;
-    }
-
-    if (count == 3)
-    {
+    if (count == 4) {
         foundFinish();
+    } else if (!isWallLeft() && left <= forward && left <= right) {
+        turnLeft();
+
+        if (direction == 0) direction = 3;
+        else direction--;
+
+        moveForward();
+
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else x--;
+
+        count++;
+    } else if (!isWallForward() && forward <= right && forward <= left) {
+        count = 0;
+        moveForward();
+
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else x--;
+
+    } else if (!isWallRight() && right <= forward && right <= left) {
+        count = 0;
+        turnRight();
+
+        if (direction == 3) direction = 0;
+        else direction++;
+
+        moveForward();
+
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else x--;
+    } else {
+        turnLeft();
+        turnLeft();
+
+        if (direction == 2) direction = 0;
+        else if (direction == 3) direction = 1;
+        else direction++;
     }
 
 }

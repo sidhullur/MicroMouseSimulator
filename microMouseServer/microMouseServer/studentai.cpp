a8685bce24d1211eb83e9250bc498f25f02742b1
sidhullur12 <51142971+sidhullur12@users.noreply.github.com>
2021-08-09 19:38:28 -0700

    Made solution more efficient and fixed logic issues

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -53,6 +53,112 @@ void microMouseServer::studentAI()
     */
 
     // Islanded Maze Solution
+
+    grid[x][y]++;
+    cout << count;
+
+
+    if(!isWallLeft()) {
+        if (direction == 0) left = grid[x-1][y];
+        else if (direction == 1) left = grid[x][y+1];
+        else if (direction == 2) left = grid[x+1][y];
+        else left = grid[x][y-1];
+    } else left = INT_MAX;
+
+    if(!isWallForward()) {
+        if (direction == 0) forward = grid[x][y+1];
+        else if (direction == 1) forward = grid[x+1][y];
+        else if (direction == 2) forward = grid[x][y-1];
+        else forward = grid[x-1][y];
+    } else forward = INT_MAX;
+
+    if(!isWallRight()) {
+        if (direction == 0) right = grid[x+1][y];
+        else if (direction == 1) right = grid[x][y-1];
+        else if (direction == 2) right = grid[x-1][y];
+        else right = grid[x][y+1];
+    } else right = INT_MAX;
+
+    if(!isWallLeft() && left <= forward && left <= right) {
+        turnLeft();
+        count = 0;
+
+        if (direction == 0) direction = 3;
+        else direction--;
+
+        moveForward();
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else x--;
+    } else if (!isWallForward() && forward <= right && forward <= left) {
+        count = 0;
+        moveForward();
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else x--;
+    } else if (!isWallRight() && right <= forward && right <= left) {
+        turnRight();
+        count++;
+
+        if (direction == 3) direction = 0;
+        else direction++;
+
+        moveForward();
+        if (direction == 0) y++;
+        else if (direction == 1) x++;
+        else if (direction == 2) y--;
+        else x--;
+    } else {
+        turnLeft();
+        turnLeft();
+        count = 0;
+        if (direction == 2) direction = 0;
+        else if (direction == 3) direction = 1;
+        else direction+=2;
+    }
+    if(count == 3) foundFinish(); // Finished if three consecutive turns right (left-hand rule)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* First Attempt of Islanded Maze:
     grid[x][y]++;
 
     if (direction == 0) {
@@ -119,5 +225,6 @@ void microMouseServer::studentAI()
         else direction++;
     }
 
-}
+    */
 
+}
